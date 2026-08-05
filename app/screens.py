e1acd167bbb21a0873dfb53cfe94b58e65a563bc
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-08-05 12:49:55 +0330

    Update screens.py

--- app/screens.py
+++ app/screens.py
@@ -1,19 +1,20 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.label import MDLabel
+
+from kivymd.uix.textfield import MDTextField
 
 from app.database import save_project, create_database
 
 
-
+# ایجاد دیتابیس در شروع برنامه
 create_database()
 
 
 class HomeScreen(Screen):
 
     def __init__(self, **kwargs):
-
         super().__init__(**kwargs)
 
         layout = MDBoxLayout(
@@ -22,35 +23,36 @@ class HomeScreen(Screen):
             spacing=20
         )
 
-
-        button = MDRaisedButton(
-            text="ایجاد پروژه جدید"
+        title = MDLabel(
+            text="Golden Studio\n\nپروژه‌ها",
+            halign="center"
         )
 
-
-        button.bind(
-            on_press=self.open_create
+        create_button = MDRaisedButton(
+            text="ایجاد پروژه جدید",
+            pos_hint={"center_x": 0.5}
         )
 
+        create_button.bind(
+            on_press=self.open_create_project
+        )
 
-        layout.add_widget(button)
+        layout.add_widget(title)
+        layout.add_widget(create_button)
 
         self.add_widget(layout)
 
 
+    def open_create_project(self, *args):
 
-    def open_create(self, *args):
-
-        self.manager.current = "create"
+        self.manager.current = "create_project"
 
 
 
 class CreateProjectScreen(Screen):
 
     def __init__(self, **kwargs):
-
         super().__init__(**kwargs)
-
 
         layout = MDBoxLayout(
             orientation="vertical",
@@ -59,41 +61,51 @@ class CreateProjectScreen(Screen):
         )
 
 
-        self.name_input = MDTextField(
+        title = MDLabel(
+            text="ایجاد پروژه جدید",
+            halign="center"
+        )
+
+
+        self.project_name = MDTextField(
             hint_text="نام پروژه"
         )
 
 
-        self.code_input = MDTextField(
+        self.python_code = MDTextField(
             hint_text="دستورات پایتون پروژه",
             multiline=True
         )
 
 
-        save = MDRaisedButton(
-            text="ذخیره پروژه"
+        save_button = MDRaisedButton(
+            text="ذخیره پروژه",
+            pos_hint={"center_x": 0.5}
         )
 
 
-        save.bind(
+        save_button.bind(
             on_press=self.save
         )
 
 
-        layout.add_widget(self.name_input)
-        layout.add_widget(self.code_input)
-        layout.add_widget(save)
+        layout.add_widget(title)
+        layout.add_widget(self.project_name)
+        layout.add_widget(self.python_code)
+        layout.add_widget(save_button)
 
 
         self.add_widget(layout)
 
 
-
     def save(self, *args):
 
         save_project(
-            self.name_input.text,
-            self.code_input.text
+            self.project_name.text,
+            self.python_code.text
         )
+
+        self.project_name.text = ""
+        self.python_code.text = ""
 
         self.manager.current = "home"
