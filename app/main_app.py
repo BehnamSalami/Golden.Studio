39a397469252db025c143f2f46847d050a3ab833
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-08-05 12:35:29 +0330

    Create app/main_app.py

---
+++ app/main_app.py
@@ -0,0 +1,31 @@
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+
+from app.screens import HomeScreen, CreateProjectScreen
+
+
+class GoldenStudioApp(MDApp):
+
+    def build(self):
+
+        self.title = "Golden Studio"
+
+        self.theme_cls.primary_palette = "Blue"
+
+        manager = ScreenManager()
+
+        manager.add_widget(
+            HomeScreen(
+                name="home"
+            )
+        )
+
+        manager.add_widget(
+            CreateProjectScreen(
+                name="create"
+            )
+        )
+
+        manager.current = "home"
+
+        return manager
